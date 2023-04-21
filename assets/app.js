fc8dc5b696d9c08d03c718c3325c36b72d56c048
Ryan Weaver <ryan@thatsquality.com>
2023-04-21 14:45:50 -0400

    Adding an importmap - just to show how things will be imported

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,13 @@
+import * as Turbo from '@hotwired/turbo';
+import './other';
+import './subdir/delayed_import';
+
+console.log('Logging Turbo', Turbo);
+
 const body = document.querySelector('body');
 const footer = document.createElement('footer');
 footer.innerHTML = 'Added via JavaScript!';
 body.appendChild(footer);
 
-import './other';
-import './subdir/delayed_import';
+
+console.log(Turbo);
