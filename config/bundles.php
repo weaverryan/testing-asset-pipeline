88f218e8480e202823c8fd3364706292e47f04b4
Ryan Weaver <ryan@thatsquality.com>
2023-04-23 16:41:41 -0400

    installing easyadmin to get a Resources/public bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
