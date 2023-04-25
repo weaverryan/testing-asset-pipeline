4fb8a3275b03e5cb5a52ff6e68d51af52258f14a
Ryan Weaver <ryan@thatsquality.com>
2023-04-25 17:02:43 -0400

    adding an importmap setup

---
+++ importmap.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'app' => [
+        'path' => 'app.js',
+    ],
+    '@popperjs/core' => [
+        'url' => 'https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js',
+    ],
+    'bootstrap' => [
+        'url' => 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js',
+    ],
+    'lodash' => [
+        'downloaded_to' => 'vendor/lodash.js',
+        'url' => 'https://ga.jspm.io/npm:lodash@4.17.21/lodash.js',
+    ],
+    '@hotwired/turbo' => [
+        'url' => 'https://ga.jspm.io/npm:@hotwired/turbo@7.3.0/dist/turbo.es2017-esm.js',
+    ],
+];
